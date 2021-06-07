576eccd1665eab54dbab9a391bc61cfe10ca5542
Erik <erik@cloudzero.com>
2021-06-07 17:41:11 -0400

    Really fix output

--- uca/features/transmit.py
+++ uca/features/transmit.py
@@ -5,20 +5,23 @@
 import os
 import sys
 
+import simplejson as json
+
 from uca.interfaces.uca_api import send_uca_events
 
 
 def transmit(uca_to_send, output_file, api_key, dry_run):
+    # order maters here, we want dry run to come first, if not that, output to file, if not that api key
     if dry_run:
         print('\nDry run, exiting')
         sys.exit()
-    elif api_key:
-        print('\nSending to API')
-        send_uca_events(api_key, uca_to_send)
     elif output_file:
         with open(os.path.expanduser(output_file), 'w') as fp:
             for line in uca_to_send:
-                fp.write(line.strip() + '\n')
+                fp.write(json.dumps(line).strip() + '\n')
         print(f'\nWrote results to {output_file}')
+    elif api_key:
+        print('\nSending to API')
+        send_uca_events(api_key, uca_to_send)
     else:
         print('\nFinished, nothing to do')
