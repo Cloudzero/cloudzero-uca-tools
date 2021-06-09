7cafa77ee8d8f5e519c5fba4ed0a9d16643c5d28
Erik <erik@cloudzero.com>
2021-06-09 08:49:49 -0400

    Off by 1 error

--- uca/interfaces/uca_api.py
+++ uca/interfaces/uca_api.py
@@ -2,6 +2,7 @@
 # Licensed under the BSD License. See LICENSE file in the project root for full license information.
 # Direct all questions to support@cloudzero.com
 import sys
+from math import ceil
 
 import requests
 from requests import HTTPError
@@ -12,7 +13,7 @@ from uca.constants import UCA_API_BATCH_SIZE
 
 def send_uca_events(api_key, uca_events):
     print(
-        f"Sending {len(uca_events)} events to UCA API in {max(round(len(uca_events) / UCA_API_BATCH_SIZE), 1)} transaction(s)")
+        f"Sending {len(uca_events)} events to UCA API in {max(ceil(len(uca_events) / UCA_API_BATCH_SIZE), 1)} transaction(s)")
     for chunk in chunks(uca_events, UCA_API_BATCH_SIZE):
         url = "https://api.cloudzero.com/unit-cost/v1/telemetry"
         payload = {"records": chunk}
@@ -28,4 +29,4 @@ def send_uca_events(api_key, uca_events):
             print(error)
             print(response.text)
             sys.exit(-1)
-    print(".Done!")
+    print("Done!")
